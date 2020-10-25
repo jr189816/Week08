b2d169c8dd0f58e851010bbf897cf38f39a33583
jr189816 <70724532+jr189816@users.noreply.github.com>
2020-10-25 18:14:33 -0400

    initial commit

---
+++ scripts/menu.js
@@ -0,0 +1,13 @@
+const toggle = document.querySelector(".nav-button");
+const menu = document.querySelector("#main-nav-list");
+
+/* Toggle mobile menu */
+function toggleMenu() {    
+    if (menu.classList.contains("active")) {             menu.classList.remove("active");                 toggle.innerHTML = "&#9776;"    
+    } 
+    else {        
+        menu.classList.add("active");         toggle.innerHTML = "X";    
+    }
+}
+
+/* Event Listener */toggle.addEventListener("click", toggleMenu, false);
